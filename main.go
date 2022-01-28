267ac459ff6ba4f5712cda60a70185b17e80f2fd
Johan H <kyotenshi@MacBook-Pro-de-Johan.local>
2022-01-28 15:55:12 +0100

    Exercice 2.2

--- main.go
+++ main.go
@@ -5,13 +5,15 @@ import (
 )
 
 func main() {
-	name := "Johny"
+	name := "Johan"
 	//CODE
-	if name == "Johan" {
-		fmt.Println("It's indeed my name :", name)
-	} else if len(name) == len("Johan") {
-		fmt.Println("It's not my name, but fun fact, we have the same number of letters !", len(name))
-	} else {
-		println("Unlucky, we have nothing in common")
+	switch {
+	case name == "Johan":
+		fmt.Println("Good guess, my name is indeed: ", name)
+	case len(name) == len("Johan"):
+		fmt.Println("Nope, it's not my name, but we have the same number of letters !", len(name))
+	default:
+		fmt.Println("Unlucky, we have nothing in common")
+
 	}
 }
