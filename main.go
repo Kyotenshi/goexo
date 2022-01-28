9d3f6cbc54e4209c78eeb6d14e10e62ced57f522
Johan H <kyotenshi@MacBook-Pro-de-Johan.local>
2022-01-28 16:26:20 +0100

    Exercice 4.3

--- main.go
+++ main.go
@@ -3,13 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(stringToBool("omfg"))
+	bool, err := fmt.Println(stringToBool("omfg"))
+
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println(bool)
+	}
 }
 
-func stringToBool(conversion string) bool {
+func stringToBool(conversion string) (bool, error) {
 	if conversion == "true" {
-		return true
+		return true, nil
+	} else if conversion == "false" {
+		return false, nil
 	} else {
-		return false
+		return false, fmt.Errorf("Cannot convert string to bool")
 	}
 }
